0f0c26e9c41b53394ad12b09cfd60b9e4ce15694
ddehueck <d.dehueck@gmail.com>
2018-09-16 20:25:45 -0400

    Add acceptance response form

--- mic-site/routes/index.js
+++ mic-site/routes/index.js
@@ -15,8 +15,63 @@ router.get('/call-for-submissions', function(req, res, next) {
 
 router.get('/call-for-submissions.html', function(req, res, next) {
 	res.redirect('/call-for-submissions');
+
 });
 
+// ACCEPTANCE FORM
+router.get('/acceptance-form', function(req, res, next) {
+	res.render('acceptance-form/acceptance-form', {
+		email: req.query.email,
+	});
+});
+
+// TODO: don't repeat this
+router.post('/acceptance-form', function(req, res, next) {
+	// RECORD INFO
+	// spreadsheet key is from the URL
+	var doc = new GoogleSpreadsheet(process.env.ACCEPTANCE_RESPONSE_SPREADSHEET_ID);
+	var sheet;
+
+	async.series([
+		function setAuth(step) {
+			var creds = require('../credentials/drive-credentials.json');
+			doc.useServiceAccountAuth(creds, step);
+		},
+
+		function getSheet(step) {
+			doc.getInfo(function(err, info) {
+				if (err) {step(err)}
+				sheet = info.worksheets[0];
+				step();
+			});
+		},
+
+		function addRowData(step) {
+			sheet.addRow(req.body, function(err, rows){
+				if (err) {step(err)}
+				step();
+			});
+		}, 
+
+		function sendConfirmationEmail(step) {
+			if (req.body.email_address != '') {
+				emailUtil.sendConfirmationEmail(req.body, function(err, info) {
+					if (err) {step(err)}
+				});
+			}
+			step();
+		},
+
+	], function(err){
+			if (err) {
+				res.send('<strong>Something went wrong. Please try filling out the form again. If the error persists please send an email to ddehueck@bu.edu with the following error information:</strong></br></br>' + err);
+			} else {
+				res.render('acceptance-form/completed');
+			}
+	});
+});
+
+// SPONSOR FORM
 router.get('/sponsor-form', function(req, res, next) {
 	res.render('sponsor-form/sponsor-form');
 });
